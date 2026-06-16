de66eb43d97a88be8c6559cba469f6bcb093154a
ibrozdev <ibraahim3523@gmail.com>
2026-06-16 08:39:26 +0300

    refactor todo app script to enhance task management and add viewing functionality

--- projects/todo-app/todo.bash
+++ projects/todo-app/todo.bash
@@ -6,9 +6,53 @@ touch "$FILE"
 
 add_task(){
     read -p "Enter task: " task;
+    if [[ -z "$task" ]]; then
+        echo "Task cannot bet empty!"
+        return
+    fi
+
+
     echo "$task" >> "$FILE";
-    echo "task added.";
+    echo "task added successfully!";
 }
 
-echo "Learn Bash" >> tasks.txt
-echo "Learn linux" >> tasks.txt
+# View all tasks
+view_tasks(){
+    echo
+    echo " ===== TASKS ===== ";
+    if [[ -s "$FILE" ]];then
+        echo " No Tasks found!"
+        return
+    fi
+    cat "$FILE";
+}
+
+# Main menu
+
+while true
+do 
+    echo 
+    echo "===== TODO APP ===== "
+    echo "1. Add Tasks "
+    echo "2. View Tasks "
+    echo "3. Exit "
+    
+    read -p "Choose option: " choice
+
+    case $choice in
+    1)
+        add_task
+        ;;
+    2)
+        view_tasks
+        ;;
+    3)
+        echo "Goodbye!"
+        break
+        ;;
+    *)
+        echo "invalid choice"
+        ;;
+  esac
+done
+
