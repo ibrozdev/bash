f483441f975fb76b65772bd52c3d58a5e4794c18
ibrozdev <ibraahim3523@gmail.com>
2026-06-16 06:55:36 +0300

    refactor calculator script to improve structure and add functionality

--- projects/calculator.bash
+++ projects/calculator.bash
@@ -1,19 +1,69 @@
 	#!/users/bin/env bash
 
-read -p "Enter first number ? " NUM1;
-read -p "Enter second number ? " NUM2;
-
-echo "choose operation: ";
-echo "1. Addition ";
-echo "2. Substraction ";
-echo "3. Multiplication ";
-echo "4. Division ";
-
-read choice;
 
 add() {
     echo "Result: $(( NUM1 + NUM2 ))";
 };
+
+subtract(){
+    echo "Result: $((NUM1 + NUM2 ))";
+}
+
+multiply(){
+    echo "Result: $((NUM1 + NUM2 ))";
+}
+
+divide() {
+    if [ "$NUM2" -eq 0 ];
+    then
+       echo "Error cannot divide by 0"
+    else
+       echo "Result: $((NUM1 / NUM2 ))";
+    fi
+};
+
+while true 
+do
+    echo
+    echo "===== calculator ====="
+    echo "1. Addition ";
+    echo "2. Subtraction ";
+    echo "3. Multiplication ";
+    echo "4. Division ";
+    echo "5. Exit ";
+    read -p "choose an option: " choice;
+
+    if [ "$choice" -eq 5 ]; 
+    then
+        echo "Goodbye!"
+        break
+    fi
+
+    read -p "Enter first number: " NUM1
+    read -p "Enter second number: " NUM2
+    
+    case $choice in
+    1)
+        add
+        ;;
+    2)
+        subtract
+        ;;
+    3)
+        multiply
+        ;;
+    4)
+        divide
+        ;;
+    *)
+        echo "Invalid choice! ";
+        ;;
+  esac
+done
+
+
+
+
 
 substract() {
     echo "Result: $(( NUM1 - NUM2 )) ";
