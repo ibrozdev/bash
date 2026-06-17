0e6430e8456bde3cabf7bd0e46d7a9ed25ed8a80
ibrozdev <ibraahim3523@gmail.com>
2026-06-17 05:49:54 +0300

    add delete task functionality to todo app script

--- projects/todo-app/todo.bash
+++ projects/todo-app/todo.bash
@@ -27,6 +27,33 @@ view_tasks(){
     cat "$FILE";
 }
 
+# delete task
+# how to run the script: bash todo.bash and then follow the instructions on the terminal
+# to delete a task. You will be prompted to enter the task number you wish to delete.
+# Make sure to enter a valid task number that corresponds to the tasks listed when you view tasks.
+delete_task(){
+    read -p "Enter task number to delete: " task_num;
+    if ! [[ "$task_num" =~ ^[0-9]+$ ]]; then
+        echo "Invalid input! Please enter a valid task number."
+        return
+    fi
+
+    if ! [[ -s "$FILE" ]]; then
+        echo "No tasks to delete!"
+        return
+    fi
+
+    total_tasks=$(wc -l < "$FILE")
+    if (( task_num < 1 || task_num > total_tasks )); then
+        echo "Invalid task number! Please enter a number between 1 and $total_tasks."
+        return
+    fi
+
+    sed -i "${task_num}d" "$FILE"
+    echo "Task deleted successfully!"
+}
+
+
 # Main menu
 
 while true
@@ -35,7 +62,8 @@ do
     echo "===== TODO APP ===== "
     echo "1. Add Tasks "
     echo "2. View Tasks "
-    echo "3. Exit "
+    echo "3. delete Tasks "
+    echo "4. Exit "
     
     read -p "Choose option: " choice
 
@@ -47,6 +75,9 @@ do
         view_tasks
         ;;
     3)
+        delete_task
+        ;;
+    4)
         echo "Goodbye!"
         break
         ;;
